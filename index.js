ae7bcef270ee4581cc3891dfe34744306538ebd0
ricklopez <ricklopez@outlook.com>
2018-09-30 10:36:10 -0400

    Add Custom Server Logic

--- index.js
+++ index.js
@@ -8,18 +8,41 @@ server.use(middlewares);
 server.use((req, res, next) => {
   console.log("In Auth Middleware");
  if (isAuthorized(req)) { // add your authorization logic here
-   next() // continue to JSON Server router
+   next(); // continue to JSON Server router
  } else {
-   res.sendStatus(401)
+   res.sendStatus(401);
  }
 });
+
+server.use(jsonServer.bodyParser);
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query);
+});
+
+server.post('/auth', (req, res) => {
+  
+  const user = authAndGetUser(req.body, req.headers);
+  
+  res.json(user);
+});
+
 
 server.use(router);
 
 server.listen(process.env.PORT || 3001, () => {
-  console.log('JSON Server is running')
+  console.log('JSON Server is running');
 });
 
 function isAuthorized(){
   return true;
+}
+
+function authAndGetUser(body, headers){
+  return {
+    username: "",
+    role:"",
+    authToken: ""
+  }
 }
